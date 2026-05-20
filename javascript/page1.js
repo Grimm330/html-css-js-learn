ebdea672e9dfffa4b8f61c6092ce0d0c3b6edb94
Grimm330 <aman.ghost330@gmail.com>
2026-05-20 12:45:09 +0600

    javascript start

---
+++ javascript/page1.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+console.log("This is my first JavaScript code.");
+//window.alert("Welcome to JavaScript programming!");
+document.getElementById("myh2").textContent = "JavaScript is fun!";
+document.getElementById("myp").textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit.";
