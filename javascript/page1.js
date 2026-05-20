103fac01bcd4be55d08bcfc7bf2406493793d532
Grimm330 <aman.ghost330@gmail.com>
2026-05-20 18:56:48 +0600

    rrf

--- javascript/page1.js
+++ javascript/page1.js
@@ -43,3 +43,63 @@ console.log(`The sum of num1 and num2 is: ${sum2}`);
 const PI = 3.14159;
 console.log(`The value of PI is: ${PI}`);
 
+//math
+
+let radius = 5.8474747466;
+let a= Math.round(radius);
+let b= Math.ceil(radius);
+let c= Math.floor(radius);
+console.log(`The rounded value of radius is: ${a}`);
+console.log(`The ceiling value of radius is: ${b}`);
+console.log(`The floor value of radius is: ${c}`);
+let e = Math.sqrt(radius);
+console.log(`The square root of radius is: ${e}`);
+let randomNum = Math.random();
+console.log(`A random number between 0 and 1 is: ${randomNum}`);
+let t= Math.pow(radius, 2);
+console.log(`The value of radius raised to the power of 2 is: ${t}`);
+let maxNum = Math.max(10, 20, 5, 15);
+console.log(`The maximum number among 10, 20, 5, and 15 is: ${maxNum}`);
+let minNum = Math.min(10, 20, 5, 15);
+console.log(`The minimum number among 10, 20, 5, and 15 is: ${minNum}`);
+
+//if statement
+let age = 18;
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+//for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(`Iteration number: ${i}`);
+}
+
+//while loop
+let count = 1;
+while (count <= 5) {
+    console.log(`Count is: ${count}`);
+    count++;
+}
+//switch statement
+let day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Today is Monday.");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday.");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday.");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday.");
+        break;
+    case "Friday":
+        console.log("Today is Friday.");
+        break;
+    default:
+        console.log("It's the weekend!");
+}   
