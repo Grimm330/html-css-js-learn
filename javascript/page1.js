c5f0263b979a17d3c7bd64f733d4cdda3564537b
Grimm330 <aman.ghost330@gmail.com>
2026-05-20 13:45:45 +0600

    basic

--- javascript/page1.js
+++ javascript/page1.js
@@ -1,5 +1,20 @@
 console.log("Hello World!");
+//output
 console.log("This is my first JavaScript code.");
 //window.alert("Welcome to JavaScript programming!");
+//pass text content to h2 and p elements
 document.getElementById("myh2").textContent = "JavaScript is fun!";
 document.getElementById("myp").textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+//variable declaration and assignment
+let x = 10;
+let y = 20;
+let z= 10.33;
+let name = "John Doe";
+let isStudent = true;
+let sum = x+(x+z);
+console.log("The sum of x and y is: " + sum);
+//2nd way to output the sum
+console.log(`The sum of x and y is: ${sum}`);
+console.log(typeof z);
+console.log(typeof name);
+console.log(typeof isStudent);
